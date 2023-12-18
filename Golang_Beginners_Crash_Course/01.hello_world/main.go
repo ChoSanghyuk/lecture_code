29032678dbf0d0cc33148a80bfa6d4be8655f106
neutron <chosh901@naver.com>
2023-12-19 08:45:56 +0900

    go start

---
+++ Golang_Beginners_Crash_Course/01.hello_world/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"myapp/doctor"
+)
+
+func main() {
+	var whatToSay string
+	whatToSay = doctor.Intro()
+	fmt.Println(whatToSay)
+}
+
+// func main() {
+
+// 	// var whatToSay string
+// 	// whatToSay = "Hello world, again!"
+// 	whatToSay := "Hello world, again!"
+
+// 	sayHelloWorld(whatToSay)
+// }
+
+// func sayHelloWorld(whatToSay string) {
+// 	fmt.Println(whatToSay)
+// }
